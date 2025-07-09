dc50c20dbcc4146d64e9d6cbfb751016a990428b
Swathi Gurijala <swathigurijala131@gmail.com>
2025-07-09 14:35:28 +0530

    Update HeroSlider.js

--- components/HeroSlider.js
+++ components/HeroSlider.js
@@ -1,1 +1,18 @@
+"use client";
+import { useState, useEffect } from "react";
+
+const slides = ["/images/hall1.jpg", "/images/wedding.jpg", "/images/birthday.jpg"];
+
+export default function HeroSlider() {
+  const [idx, setIdx] = useState(0);
+  useEffect(() => {
+    const iv = setInterval(() => setIdx((i) => (i + 1) % slides.length), 5000);
+    return () => clearInterval(iv);
+  }, []);
+  return (
+    <div className="h-64 md:h-96 overflow-hidden relative">
+      <img src={slides[idx]} alt={`slide-${idx}`} className="w-full h-full object-cover transition-opacity duration-1000" />
+    </div>
+  );
+}
 
