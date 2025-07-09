8587d19314e54f739b9377aeae8382be48329982
Swathi Gurijala <swathigurijala131@gmail.com>
2025-07-09 14:37:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
 
