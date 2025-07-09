c2e66edb5d83ea7c034528e236cea4fd5fb5bead
Swathi Gurijala <swathigurijala131@gmail.com>
2025-07-09 14:37:47 +0530

    Update razorpay.js

--- razorpay.js
+++ razorpay.js
@@ -1,1 +1,10 @@
+export function loadRazorpay() {
+  return new Promise((resolve) => {
+    if (window.Razorpay) return resolve();
+    const s = document.createElement("script");
+    s.src = "https://checkout.razorpay.com/v1/checkout.js";
+    s.onload = () => resolve();
+    document.body.appendChild(s);
+  });
+}
 
