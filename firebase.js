be1562ac4a854c9e6812c98f5cb3310511e31712
Swathi Gurijala <swathigurijala131@gmail.com>
2025-07-10 19:09:51 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,17 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyDTirXHgdQee9DiTa5GG4Rx7OuJ3DFZHFM",
+  authDomain: "crodlin-halls.firebaseapp.com",
+  projectId: "crodlin-halls",
+  storageBucket: "crodlin-halls.appspot.com", // ✅ fixed this line
+  messagingSenderId: "392734865933",
+  appId: "1:392734865933:web:598c7fe39d058826a0b344"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
 
+export default app;
